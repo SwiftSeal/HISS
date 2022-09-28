feba9074d6c16b1f399eed5e946e3be9b7e525f7
Thomas Adams <thomas.adams@hutton.ac.uk>
2022-09-28 14:33:02 +0100

    Prepares function to parse CLI arguments

--- smrtrenseq_assembly/workflow/scripts/add_flanking_to_bed.py
+++ smrtrenseq_assembly/workflow/scripts/add_flanking_to_bed.py
@@ -6,3 +6,16 @@
 
 from collections import defaultdict
 import argparse
+
+# Prepare function to parse CLI arguments
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Add flanking seq to bed')
+    parser.add_argument('--input_bed', required=True,
+                        help='Input file of predicted NLRs in bed format')
+    parser.add_argument('--input_fasta', required=True,
+                        help='Input fasta file of assembled contigs')
+    parser.add_argument('--output', required=True,
+                        help='Output file in bed format with flanking added')
+    return parser.parse_args()
