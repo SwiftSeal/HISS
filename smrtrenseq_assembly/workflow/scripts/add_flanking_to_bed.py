4b672ed0ce69eb29522636dfbd85210729ec63cc
Thomas Adams <thomas.adams@hutton.ac.uk>
2022-09-28 15:55:25 +0100

    Prepares function to load contig lengths into dictionary

--- smrtrenseq_assembly/workflow/scripts/add_flanking_to_bed.py
+++ smrtrenseq_assembly/workflow/scripts/add_flanking_to_bed.py
@@ -14,8 +14,22 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Add flanking seq to bed')
     parser.add_argument('--input_bed', required=True,
                         help='Input file of predicted NLRs in bed format')
-    parser.add_argument('--input_fasta', required=True,
-                        help='Input fasta file of assembled contigs')
+    parser.add_argument('--input_lengths', required=True,
+                        help='Input file of contig lengths')
     parser.add_argument('--output', required=True,
                         help='Output file in bed format with flanking added')
     return parser.parse_args()
+
+
+# Prepare function to get contig lengths:
+
+
+def get_lengths(lengths: list):
+    contig_dict = defaultdict(float)
+    for line in lengths:
+        line = line.rstrip()
+        split_line = line.split('\t')
+        contig = split_line[0]
+        length = split_line[1]
+        contig_dict[contig] = length
+    return contig_dict
